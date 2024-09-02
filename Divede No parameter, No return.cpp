a66e44fd1de85f09b004ab66acc5635143f47746
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-02 20:58:56 +0530

    Divede No parameter, No return.

---
+++ Divede No parameter, No return.cpp
@@ -0,0 +1,18 @@
+// No parameter, No return
+
+#include<iostream>
+using namespace std;
+
+void add(){
+	double num1, num2,divide;
+	cout<<"Enter first number : ";
+	cin>>num1;
+	cout<<"Enter second number : ";
+	cin>>num2;
+	divide= num1/num2;
+	cout<<"Divide is : "<<divide;
+}
+
+int main(){
+  add();
+}
