1f4f483cf8d83df4071512a208e0a66ebb342197
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-02 20:58:10 +0530

    Add two number using function parameter ,no return

---
+++ Add two number using function parameter ,no return.cpp
@@ -0,0 +1,18 @@
+// parameter, no return   
+#include<iostream>
+using namespace std;
+
+void add(int a,int b){
+	cout<<"Sum of two number is : " <<a+b;
+}
+
+int main(){
+	int num1, num2;
+	cout<<"Enter first number : ";
+	cin>>num1;
+	cout<<"Enter second number : ";
+	cin>>num2;
+	
+	add(num1,num2);
+
+}
