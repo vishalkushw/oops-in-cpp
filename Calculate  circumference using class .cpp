ba82c74639eb4b6e22c030079e9ad012cc5d1b0f
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-12 19:02:33 +0530

    Calculate circumference using class

---
+++ Calculate  circumference using class .cpp
@@ -0,0 +1,33 @@
+//1)Write a C++ program to define a class named Circle.
+//private member: radius
+//public member functions:calculate_area(),calculate_circumference()
+
+
+#include <iostream>
+using namespace std;
+
+class Circle{
+	private :
+		double radius;
+	
+	public:
+		Circle (double r){
+			radius=r;
+		}
+		
+	 double calculate_area(){
+	 	return 3.1415*radius*radius;
+	 	
+	 }	
+	 
+	double calculate_circumference(){
+		return 2*3.1415*radius;
+		
+	} 
+};
+int main(){
+	Circle c(6);
+	cout<<"Area : "<<c.calculate_area()<<endl;
+	cout<<"Circumference : "<<c.calculate_circumference();
+	
+}
