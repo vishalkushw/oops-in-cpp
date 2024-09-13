da6fd6b279a0d2d4e0f4cb6b90b3ef06691cafcd
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-14 00:06:28 +0530

    Default Constructor

---
+++ Default Constructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+using namespace std;  
+class Employee  
+ {  
+   public:  
+        Employee()    
+        {    
+            cout<<"Default Constructor "<<endl;    
+        }    
+};  
+int main(void)   
+{  
+    Employee e1; 
+    Employee e2;    
+}  
