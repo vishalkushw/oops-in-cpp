d3036811a4b7c149a146c0b2c43beff881234c1a
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-16 21:15:25 +0530

    Operater overloding

---
+++ Operater overloding.cpp
@@ -0,0 +1,31 @@
+//operator overloading 
+
+#include<iostream>
+using namespace std;
+
+class Operators{
+    private:
+     int marks;
+     
+    public:
+     Operators(int num){
+     marks=num;
+   }
+   
+  Operators operator +(Operators obj){
+    Operators temp(0);
+    temp.marks=marks+obj.marks;
+    return temp;
+  }
+  
+  void display(){
+    cout<<marks;
+
+  }
+};
+
+int main(){
+Operators s1(21),s2(53),s3(31);
+Operators result=s1+s2+s3;
+result.display();
+}
