d547b29e39b91b6986ca5d17da87be706858a0ab
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-16 21:12:45 +0530

    Bank class and objec

---
+++ Bank class and object.cpp
@@ -0,0 +1,47 @@
+
+#include<iostream>
+using namespace std;
+
+class Bank{
+    private:
+       string name;
+	   string type_of_account;
+       int account_number,password,balance;
+
+    public:
+        Bank(string nm,int ac,string ty,int bal,int pass){
+        name=nm;
+        account_number=ac;
+        type_of_account=ty;
+        balance=bal;
+        password=pass;
+}
+
+void deposit(int amount,int pass){
+    if(password==pass){
+        balance=balance+amount;
+    }
+    else{
+        cout<<"wrong password"<<endl;
+    }
+}
+void withdraw(int amount,int pass){
+    if(password==pass){
+        balance =balance-amount;
+    }
+}
+
+void display(){
+    cout<<"Name "<<name<<endl;
+    cout<<"Balance "<<balance<<endl;
+}
+};
+int main(){
+    Bank obj1("Vishal",786765,"saving",2300,432);
+    obj1.display();
+    obj1.deposit(4000,432);
+    obj1.display();
+    obj1.withdraw(500,438);
+    obj1.display();
+    
+}
