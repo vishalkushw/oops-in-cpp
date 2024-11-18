12f7f79073a77a18ac65838587b45166a11bfd38
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-18 12:12:06 +0530

    OOps

---
+++ OOPS/class object.cpp
@@ -0,0 +1,38 @@
+/*type of classes 
+#include <iostreame>
+using namespace std;
+class classname
+{
+	datatype variable;
+	public : datatype functionname(parameter /non parameter)
+	{
+		statements/code;
+	}
+};
+int main()
+{
+	classname objectname;
+	objectname.functionname();
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+class atm
+{
+	int rs =200;
+	public :void show()
+	
+	{
+		cout<<rs;
+	}
+};
+int main()
+{
+	atm sbi;
+	sbi.show();
+}
+
+
+
