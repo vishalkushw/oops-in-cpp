6a129102b8f0037b38038bec3ff721fe5a861ef9
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-18 12:17:29 +0530

    sbi

---
+++ OOPS/sbi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class atm
+{
+	int rs =200;
+	public :void show()
+	
+	{
+		cout<<rs;
+	}
+	
+	public :void fun(){
+		cout<<"\nwellcome to sbi";
+	}
+};
+int main()
+{
+	atm sbi;
+	sbi.show();
+	sbi.fun();
+}
+
+
+
