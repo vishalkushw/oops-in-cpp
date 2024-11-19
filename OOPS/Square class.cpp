5f75a0cf8bdfbf14a085c75342f55d2e67cbdaab
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-19 11:52:35 +0530

    Square class

---
+++ OOPS/Square class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Sqare{
+	int num=4;
+	public : void sqr()
+	{
+	 cout<<"Square Number : "<<num*num;
+	}
+	
+};
+
+int main(){
+	Sqare num;
+	num.sqr();
+	
+}
