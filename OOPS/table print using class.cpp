3f409710a7d7fc61d5dcadfa86f7dd865d65a0e7
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-26 12:14:02 +0530

    table print using class

---
+++ OOPS/table print using class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class top
+{
+	public:static void table(int a)
+	{
+		for(int i=1;i<=10;i++){
+			cout<<a*i<<"\n";
+		}
+	}
+};
+
+int main(){
+	top::table(5);
+	
+}
