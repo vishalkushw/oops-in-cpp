2712d9378886d1578fccbb331b039d1c03452fdc
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-30 19:36:14 +0530

    class code cpp

---
+++ OOPS/july24pointer.cpp
@@ -0,0 +1,40 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	char msg[]="pineapple";
+	int count=0;
+	char *ptr=msg;
+	while(*ptr!='\0'){
+		count++;
+		ptr++;
+	}
+	cout<<"number of characters : "<<count<<endl;
+}
+*/
+/*
+//refrence identif
+#include <iostream>
+ using namespace std;
+ int main(){
+
+   int  number1=45, number2=50;
+   int &digit1=number1;
+    digit1=900;
+    cout<<number1;   
+}
+*/
+/*
+#include <iostream>
+ using namespace std;
+ int main(){
+
+   int  number1=45, number2=50;
+   int &digit1=number1;
+    digit1=number2;
+    digit1=666;
+    cout<<number1<<" "<<number2;   
+}
+*/
+
+
