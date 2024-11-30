1284cd560a95a536e05cca1312a6f9dc39e7b23b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-30 19:35:35 +0530

    class code cpp

---
+++ OOPS/july23ptr.cpp
@@ -0,0 +1,80 @@
+
+//  Pointer arthmathic
+
+// WAP to find the highest between 2 using pointer
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"enter number : ";
+	cin>>num1>>num2;
+	
+	int *p1= &num1;
+	int *p2= &num2;
+	
+	if(*p1>*p2){
+    cout<<"pointer p1 is highest:"<<*p1;
+	}
+	else{
+	cout<<"pointer p2 is highest:"<<*p2;	
+	}
+
+}
+
+*/
+
+// WAP to add 2 numbers using pointer
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"enter number : ";
+	cin>>num1>>num2;
+	
+	int *p1= &num1;
+	int *p2= &num2;
+	
+	cout<<*p1+*p2;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	int num1=90,num2=88;
+	
+	int *p1= &num1;
+	int *p2= &num2;	
+	
+	cout<<"address of number 1 : "<<p1<<endl;
+	cout<<"address of number 2 : "<<p2<<endl;
+	
+	p2=p2+1;
+	cout<<"address of number 1 : "<<p1<<endl;
+	cout<<"address of number 2 : "<<p2<<endl;
+	cout<<"Add of both : "<<*p1+*p2<<endl;
+	
+	p1=p1-1;
+	cout<<"address of number 1 : "<<p1<<endl;
+	cout<<"address of number 2 : "<<p2<<endl;
+	cout<<"Add of both : "<<*p1+*p2<<endl;
+	
+}
+*/
+
+
+
+/*
+// WAP to calculate the length of a string using a pointer.
+#include <iostream>
+using namespace std;
+int main(){
+   char name[]="vishal";  
+	char *p1= &name;
+	int count=0;
+	
+	cout<<;
+}
+*/
