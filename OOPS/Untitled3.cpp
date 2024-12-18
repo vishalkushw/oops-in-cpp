f3f427ea42ecdfd52a12db0da13c704b1eb7e1fc
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-18 22:06:16 +0530

    class code cpp

---
+++ OOPS/Untitled3.cpp
@@ -0,0 +1,11 @@
+/* Dimond problem 
+In a multipul inheritance there may be a dimond problem i n both inherited classes hacving the
+same signature the function
+ 
+*/ 
+
+/*Hirarcical  in a heracial there in  only one parent class and multiple children classes and
+eatch class inheritais the property of root class      
+
+*/
+ 
